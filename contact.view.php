d75adb017a4618d7203a5b1dd78552b350024448
Mark Lannen <marklannendev@gmail.com>
2024-02-12 21:46:29 -0700

    add about and contact page and their views

--- contact.view.php
+++ contact.view.php
@@ -127,12 +127,12 @@
 
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact Us</h1>
         </div>
     </header>
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p>Welcome to the home page.</p>
+            <p>Welcome to the contact page.</p>
         </div>
     </main>
 </div>
