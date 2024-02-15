1247536456b64a5ec1a1b73572a80df8a812b494
Mark Lannen <marklannendev@gmail.com>
2024-02-14 21:09:02 -0700

    add partials and dynamic h1 in header

---
+++ views/partials/head.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html class="h-full bg-gray-100" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="h-full">
+<div class="min-h-full">
